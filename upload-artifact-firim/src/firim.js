a80b0d1c13074732cb00d2841e36e6cea91a31bd
QuincySx <772804430@qq.com>
2021-07-12 20:04:14 +0800

    add upload timeout

--- upload-artifact-firim/src/firim.js
+++ upload-artifact-firim/src/firim.js
@@ -1,5 +1,11 @@
 var needle = require('needle');
 
+needle.defaults({
+    open_timeout: 5 * 60 * 1000,
+    read_timeout: 5 * 60 * 1000,
+    response_timeout: 10 * 60 * 1000
+})
+
 function get_blob_by_base64(dataURI) {
     var base64Data = dataURI.replace(/^data:image\/\w+;base64,/, '')
     return Buffer.from(base64Data, 'base64')
@@ -14,14 +20,16 @@ function upload_icon_file(param, base64) {
     try {
         var file_buffer = get_blob_by_base64(base64)
         var file_type = base64.split(';')[0].replace('data:', '')
+
         var data = {
             "key": param.key,
             "token": param.token,
-            "file": {
+            file: {
                 buffer: file_buffer,
                 content_type: file_type
             }
         }
+
         return upload_file(param.upload_url, data)
     } catch (error) {
         console.error(error)
@@ -36,7 +44,7 @@ function upload_binary_file(param, artifact_info, file_path) {
         "x:build": artifact_info.versionCode,
         "x:version": artifact_info.versionName,
         "x:changelog": artifact_info.changelog,
-        "file": { file: file_path, content_type: 'multipart/form-data' }
+        file: { file: file_path, content_type: 'multipart/form-data' }
     }
 
     if (artifact_info.release_type) {
@@ -46,7 +54,7 @@ function upload_binary_file(param, artifact_info, file_path) {
     return upload_file(param.upload_url, data)
 }
 
-exports.upload_artifact = async function (params, artifact_info, upload_file_path, use_release_id) {
+exports.upload_artifact = async function (params, artifact_info, upload_file_path, custom_host, use_release_id) {
     try {
         console.log("Begin upload icon to firim")
         await upload_icon_file(
@@ -59,23 +67,37 @@ exports.upload_artifact = async function (params, artifact_info, upload_file_pat
     console.log("Upload icon success")
 
     console.log("Begin upload artifact to firim")
-    return upload_binary_file(
+    let result = await upload_binary_file(
         params.cert.binary,
         artifact_info,
         upload_file_path
     )
-        .then((result) => {
-            console.log("Upload artifact success")
+    console.info(result)
+    console.log("Upload artifact success")
 
-            var download_url = "http://" + params.download_domain + "/" + params.short
-            if (use_release_id) {
-                download_url = download_url + "?release_id=" + result.release_id
-            }
+    let download_url = undefined
+    if (custom_host) {
+        download_url = custom_host + "/" + params.short
+    } else {
+        let download_prefix = "http://"
+        if (params.download_domain_https_ready) {
+            download_prefix = "https://"
+        }
+        download_url = download_prefix + params.download_domain + "/" + params.short
+    }
 
-            return {
-                "download_url": download_url
-            }
-        })
+    if (use_release_id && result.release_id) {
+        download_url = download_url + "?release_id=" + result.release_id
+    }
+
+    if (!result.download_url) {
+        throw result
+    }
+
+    console.log("download_url: " + download_url)
+    return {
+        "download_url": download_url
+    }
 }
 
 exports.get_api_token = function (artifact_info, api_token) {
--- upload-artifact-firim/src/index.js
+++ upload-artifact-firim/src/index.js
@@ -9,6 +9,7 @@ async function main() {
     const input_version_suffix = core.getInput('version-suffix', { required: false })
     const input_use_release_id = core.getInput('use-release-id', { required: false }) || true
     const input_ios_release_type = core.getInput('ios-release-type', { required: false }) || "Adhoc"
+    const input_custom_host = core.getInput('custom-host', { required: false })
 
     console.log("Begin parse artifact info")
     console.log("Parsing file path: " + input_upload_file_path)
@@ -27,7 +28,7 @@ async function main() {
     console.info("Parser artifact info:\n" + JSON.stringify(artifact_info))
 
     let token = await firim.get_api_token(artifact_info, input_api_token)
-    let upload_result = await firim.upload_artifact(token, artifact_info, input_upload_file_path, input_use_release_id)
+    let upload_result = await firim.upload_artifact(token, artifact_info, input_upload_file_path, input_custom_host, input_use_release_id)
 
     core.setOutput('artifact-download-url', upload_result && upload_result.download_url || '');
     core.setOutput('artifact-type', artifact_info && artifact_info.platform || '');
