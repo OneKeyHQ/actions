356ec17fc8289466d26b7c837559592982b33471
Leon <lixiao.dev@gmail.com>
2024-03-06 18:04:10 +0800

    feat: Add cloudflare-worker-webhooks action (#52)
    * feat: Add cloudflare-worker-webhooks action
    
    * Add validation for required fields in webhook configuration
    
    * Add secret key input for Github action signature

---
+++ cloudflare-worker-webhooks/src/index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const axios = require('axios');
+const crypto = require('crypto');
+
+async function run() {
+  try {
+    const webhookUrl = core.getInput('web-hook-url');
+    const secretKey = core.getInput('secret-key');
+    const data = {
+      platform: core.getInput('artifact-type'),
+      artifactName: core.getInput('artifact-name'),
+      bundleId: core.getInput('artifact-bundle-id'),
+      versionName: core.getInput('artifact-version-name'),
+      buildNumber: core.getInput('artifact-version-code'),
+      downloadUrl: core.getInput('artifact-download-url'),
+    };
+
+
+    if (!webhookUrl) {
+      core.setFailed('web-hook-url is required');
+      return
+    }
+
+    if (!secretKey) {
+      core.setFailed('secret-key is required');
+      return
+    }
+
+    const dataString = JSON.stringify(data);
+    const hash = crypto.createHmac('sha1', secretKey)
+    .update(dataString)
+    .digest('hex');
+
+    const headers = {
+      'github-action-signature': `sha1=${hash}`
+    };
+
+    await axios.post(webhookUrl, data, { headers });
+
+    console.log('Notification sent successfully');
+  } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
+  }
+}
+
+run();
