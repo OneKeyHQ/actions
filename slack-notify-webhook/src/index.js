e95399f91eeae98141aa54469d99f333a0ac0582
Leon <lixiao.dev@gmail.com>
2026-03-16 16:34:31 +0800

    feat: add bundleVersion, skipGpg, buildSource inputs
    Add 3 optional inputs to support differentiated Slack notifications
    for bundle-release vs daily-build workflows.

--- slack-notify-webhook/src/index.js
+++ slack-notify-webhook/src/index.js
@@ -13,6 +13,9 @@ async function run() {
       versionName: core.getInput('artifact-version-name'),
       buildNumber: core.getInput('artifact-version-code'),
       downloadUrl: core.getInput('artifact-download-url'),
+      bundleVersion: core.getInput('artifact-bundle-version'),
+      skipGpg: core.getInput('artifact-skip-gpg'),
+      buildSource: core.getInput('artifact-build-source'),
     };
 
 
