86ad045e18da7958f659995ee2f9df375dd03ef4
Leon <lixiao.dev@gmail.com>
2026-03-18 19:05:45 +0800

    feat: add bundle-release notification inputs to slack-notify-webhook (#84)
    * feat: add bundleVersion, skipGpg, buildSource inputs
    
    Add 3 optional inputs to support differentiated Slack notifications
    for bundle-release vs daily-build workflows.
    
    * chore: yarn lock
    
    * feat: add branch and commit passthrough to slack-notify-webhook action

--- slack-notify-webhook/src/index.js
+++ slack-notify-webhook/src/index.js
@@ -13,6 +13,11 @@ async function run() {
       versionName: core.getInput('artifact-version-name'),
       buildNumber: core.getInput('artifact-version-code'),
       downloadUrl: core.getInput('artifact-download-url'),
+      bundleVersion: core.getInput('artifact-bundle-version'),
+      skipGpg: core.getInput('artifact-skip-gpg'),
+      buildSource: core.getInput('artifact-build-source'),
+      branch: core.getInput('artifact-branch'),
+      commit: core.getInput('artifact-commit'),
     };
 
 
